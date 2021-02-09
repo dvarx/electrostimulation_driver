fa1d54b094860930ae296af3ec01abc2efa59821
dvarx <59966971+dvarx@users.noreply.github.com>
2021-02-09 11:54:25 +0100

    added option to set resonant frequency via UART

--- pwm_configuration/state_machine.h
+++ pwm_configuration/state_machine.h
@@ -15,12 +15,14 @@ enum system_state{INIT,CLOSED_LOOP,CL_TO_RES,RES_TO_CL,RESONANT};
 
 extern char input_buffer[];
 extern uint32_t res_freq;
+inline void set_pwm_freq_res(int freq);
 
 const char SET_FREQ_CMD[]="!frq";
 
 void parse_input(uint8_t buffer_size){
     if(!strncmp(input_buffer,SET_FREQ_CMD,sizeof(SET_FREQ_CMD)/sizeof(char)-1)){
         res_freq=(uint32_t)atoi(input_buffer+sizeof(SET_FREQ_CMD)/sizeof(char)-1);
+        set_pwm_freq_res(res_freq);
     }
 }
 
